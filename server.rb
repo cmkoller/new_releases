724171511b93b04c7b4ca187ea42ae3431dff823
cmkoller <cmkoller01@gmail.com>
2015-02-24 15:08:35 -0500

    Created index page for albums

---
+++ server.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+require 'csv'
+require 'pry'
+
+def read_albums_from(csv)
+  # Desired output:
+  # { "1989" => {name: "1989", artists: ""}, "album_name" => { ... } }
+  albums = {}
+
+  CSV.foreach(csv, headers: true) do |row|
+    album_info = row.to_hash
+    albums[album_info["name"]] = album_info
+  end
+
+  albums
+end
+
+get '/albums' do
+  albums = read_albums_from('albums.csv')
+
+  erb :'/albums/index', locals: {albums: albums}
+end
